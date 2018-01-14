e59cb432c7fd6a60463dc171657e49fa2d22395a
rinsuki <428rinsuki+git@gmail.com>
2018-01-15 05:51:19 +0900

    ちゃんとMusicPlayerを選択できるように

--- src/client/components/source.tsx
+++ src/client/components/source.tsx
@@ -6,23 +6,30 @@ interface Props {
     onChanged: (music: Music) => void
 }
 
-export default class Source extends React.Component<Props> {
+interface State {
+    selectedPlayer: string
+}
+
+export default class Source extends React.Component<Props, State> {
     render() {
-        var options = config.players.map(player => {
+        var options = Object.values(config.players).map(player => {
             return <option value={player.getUniqueKey()}>{player.getDisplayName()}</option>
         })
         return (
             <div>
                 player: 
-                <select>
+                <select onChange={this.onChangePlayer.bind(this)}>
                     {options}
                 </select>
                 <button onClick={this.changeEvent.bind(this)}>Reload Music Data</button>
             </div>
         )
     }
+    onChangePlayer(e: any) {
+        this.setState({selectedPlayer: e.target.value as string})
+    }
     async changeEvent() {
-        const player = config.players[0]
+        const player = config.players[(this.state || {}).selectedPlayer] || Object.values(config.players)[0]
         if (!player) return
         const music = await player.getNowplaying()
         if (!music) return
